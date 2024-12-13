d5a03447b1927d08c8ae047d22947cac87a2f795
Rob Dawson <rdawson164@gmail.com>
2024-12-13 02:29:50 +0000

    added serializers and urls to reipie project

--- recipeproject/recipes/views.py
+++ recipeproject/recipes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+class RecipeList(APIView):
+    def get(self, request):
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
